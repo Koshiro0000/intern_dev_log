142b976344cd1f185c969b560b7ca1ba4e86af1c
h.koshiro <h.koshiro@mac0658.np.co.jp>
2025-07-11 12:56:26 +0900

    add 1_array.rb２

--- Ruby_shiren/1_array.rb
+++ Ruby_shiren/1_array.rb
@@ -5,6 +5,17 @@ def sum_array(numbers)
   numbers.sum
 end
 
+sum = numbers.sum
+puts "Sum: #{sum}"
+
+
+# 数値を配列で受け取り、平均値を計算する
+numbers = [123, 456, 789, 234, 567, 789]
+
+def sum_array(numbers)
+  numbers.sum
+end
+
 sum = sum_array(numbers)
 average = sum / numbers.size
 
